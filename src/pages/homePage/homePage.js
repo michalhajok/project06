2607acbc82817fb59d5fb13ae8048624a332de6e
michalhajok <hajokmichal@gmail.com>
2020-07-12 14:37:50 +0200

    homepage

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,10 +1,31 @@
 import React from 'react'
+
 import Nav from '../../components/organisms/nav/nav'
+
+import './home.scss'
 
 const HomePage = () => {
     return (
-        <div>
-            home
+        <div className="homePage">
+            <div className="content">
+                <article>
+                    <p>
+                        App was created in Create-React-App.
+                        In this React App I used Api, Sass, Mirage JS.
+                        Routing was created in Reach Router.
+                        Icons in site is from FontAwesome.
+                        Colors in the page you can find on <i>https://flatuicolors.com</i>.
+                    </p>
+                </article>
+                <article>
+                    <h3>About site</h3>
+                    <p>
+                        In this page you can create workout and save your results. 
+                        Also in the site you can know about exercise in gym. 
+                        What is the exercise category and what equipment will you use for the exercise.
+                    </p>
+                </article>
+            </div>
             <Nav />            
         </div>
     )
